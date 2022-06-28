5e9432833741bf07f54ea46528c82c65a7a3ef9e
pentius00 <pentius00@gmail.com>
2022-06-28 15:25:38 -0400

    added files

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import alpaca_trade_api  as tradeapi
+import config, json, request
+
+app = Flask(__name__)
+
+api = tradeapi.REST(config.API_KEY, config.API_SECRET, base_url'https://paper-api.alpaca.markets')
+
+@app.route('/')
+def dashboard():
+    orders = api.list_orders
+    
+    return render_template('dashbooard.html' alpaca_orders=orders)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    webhook_message = json.loads(request.data)
+
+    if webhook_message['passphrase'] != config.WEBHOOK_PASSPHRASE:
+        return {
+            'code': 'error',
+            'message': 'nice try buddy'
+        }
+    
+    price = webhook_message['strategy']['order_price']
+    quantity = webhook_message['strategy']['order_contracts']
+    symbol = webhook_message['ticker']
+    side = webhook_message['strategy']['order_action']
+    
+    order = api.submit_order(symbol, quantity, side, 'limit', 'gtc', limit_price=price)
+
+    return webhook_message
